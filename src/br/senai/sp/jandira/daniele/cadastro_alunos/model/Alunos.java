0303f4dd7b5e607562652a57ee360f19666e0dfd
Daniele Silva Santos <daniiellesantos100@gmail.com>
2025-12-28 19:37:44 -0300

    Componentes adicionados

--- src/br/senai/sp/jandira/daniele/cadastro_alunos/model/Alunos.java
+++ src/br/senai/sp/jandira/daniele/cadastro_alunos/model/Alunos.java
@@ -1,4 +1,17 @@
 package br.senai.sp.jandira.daniele.cadastro_alunos.model;
 
 public class Alunos {
+    public String nome;
+    public String curso;
+    public String matricula;
+    public String email;
+    public String nota;
+
+    public void exibirAluno () {
+        System.out.println("Nome: " + nome);
+        System.out.println("Curso de Ensino: " + curso);
+        System.out.println("Matrícula: " + matricula);
+        System.out.println("Email: " + email);
+        System.out.println("Nota final: " + nota);
+    }
 }
